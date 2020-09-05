55d42f4473dfc72f932a3d636b43c9a102e559cc
devinjcoding <70809043+devinjcoding@users.noreply.github.com>
2020-09-05 16:48:05 -0500

    Attempted to Implent a video feature backed out

--- src/main.java
+++ src/main.java
@@ -2,7 +2,7 @@ import java.io.FileNotFoundException;
 
 public class main {
     public static void main(String[] args) throws FileNotFoundException {
-        GameMaster gM = new GameMaster();
+        GameMasterGUI gM = new GameMasterGUI("Game Master");
         //TODO add new user class
         QuizIntake user1 = new QuizIntake("Virtual 12th Man");
         //TODO compare user class to GM
