9e34eb056f4f693dda2443a4e183175affc5232a
devinjcoding <70809043+devinjcoding@users.noreply.github.com>
2020-09-05 19:20:19 -0500

    Working on constant running

--- src/main.java
+++ src/main.java
@@ -7,13 +7,18 @@ public class main {
         HashMap<Integer, PlayType> pLog = new HashMap<>();
         GameMasterGUI frame = new GameMasterGUI("Game Master", pLog);
         QuizIntake user1Quiz = new QuizIntake("Virtual 12th Man",frame.isPlayStarted());
-        User one = new User(1,0);
+        User one = new User(1,0,user1Quiz);
         frame.setVisible(true);
         user1Quiz.setVisible(true);
         pLog.put(one.getID(),user1Quiz.getPlayType());
-        System.out.println(pLog);
         while(true){
-            user1Quiz.setPlayStarted(frame.isPlayStarted());
+            if(frame.isPlayStarted()){
+                user1Quiz.setVisible(false);
+                user1Quiz.clearInfo();
+            }else if(!frame.isPlayStarted()){
+                user1Quiz.setVisible(true);
+                one.setPlayType(user1Quiz.getPlayType());
+            }
         }
         //TODO score all user class to GM
         //TODO award points to USER
